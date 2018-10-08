1249bfacf2c7b759ffbb840a55ed1a5e00cef9b7
Eugene Fedotov <eugenefedoto@gmail.com>
2018-10-07 21:41:01 -0500

    Fix the margins and overlays.

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,48 @@
 <template>
   <div id="app">
-    <router-view/>
-    <div class="overlay">
-        <div class="wrapper">
-            <router-link to="/" id="logo">ef_</router-link>
-            <nav class="tabs">
-                <div class="selector"></div>
-                <router-link to="/projects" class="uppercase">projects</router-link>
-                <router-link to="/hacking" class="uppercase">misc</router-link>
-            </nav>
-        </div>
-        <div class="desc">Eugene Fedotov</div>
-        <div class="desc">is a <span class="uppercase subject">hacker</span></div>
-        <div class="desc">is a <span class="uppercase subject">developer</span></div>
-        <div class="desc">loves <span class="uppercase subject">anime</span></div>
+    <div class="wrapper-nav">
+        <router-link to="/" id="logo">ef_</router-link>
+        <nav class="tabs">
+            <div class="selector"></div>
+            <router-link to="/projects" class="uppercase">projects</router-link>
+            <router-link to="/hacking" class="uppercase">misc</router-link>
+        </nav>
     </div>
+    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
 @import "~normalize.css";
-.overlay {
+.wrapper-nav {
   position: absolute;
   font-family: Nobile, sans-serif;
   font-size: 2rem;
-  margin-top: 10px;
-  margin-left: 50px;
-  .desc {
-    font-size: 2em;
-    color: white;
-    .uppercase {
-      text-transform: uppercase;
-    }
+  z-index: 1;
+  display: flex;
+  margin-top: 5vh;
+  margin-left: 1vw;
+  #logo {
+    margin: 0 2vw;
   }
-  .wrapper {
-    display: flex;
-    margin-top: 1rem;
-    margin-bottom: 2rem;
-    #logo {
-      margin: 0 1.5rem;
-    }
-    .tabs {
-      font-size: 1.1rem;
-      background: #e2e2e2;
-      box-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, 0.1);
-      border-radius: 0 1rem 1rem 0;
-      a {
-        text-decoration: none;
-        color: #777;
-        text-transform: uppercase;
-        padding: 0.5em;
-        display: inline-block;
-        &:hover{
-          color: #ff006a;
-        }
+  .tabs {
+    font-size: 1.1rem;
+    background: #e2e2e2;
+    box-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, 0.1);
+    border-radius: 0 1rem 1rem 0;
+    a {
+      text-decoration: none;
+      color: #777;
+      text-transform: uppercase;
+      padding: 0.5em;
+      display: inline-block;
+      &:hover {
+        color: #ff006a;
       }
     }
   }
-  .subject,
+}
+.subject,
   #logo {
     text-decoration: none;
     background: linear-gradient(to right, #30cfd0 0%, #6410cc 100%);
@@ -65,5 +50,11 @@
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
   }
-}
+  .desc {
+    font-size: 2em;
+    color: white;
+    .uppercase {
+      text-transform: uppercase;
+    }
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="home">
+  <div id="home">
+    <div class="wrapper-desc">
+        <div class="desc">Eugene Fedotov</div>
+        <div class="desc">is a <span class="uppercase subject">hacker</span></div>
+        <div class="desc">is a <span class="uppercase subject">developer</span></div>
+        <div class="desc">loves <span class="uppercase subject">anime</span></div>
+    </div>
     <video id="hero" autoplay="autoplay" loop="loop" muted="muted">
         <source src="@/assets/hero.webm" type="video/webm">
         <source src="@/assets/hero.mp4" type="video/mp4">
@@ -8,23 +14,32 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
 export default class Home extends Vue {}
 </script>
 
-<style lang="scss">
-#hero {
+<style lang="scss" scoped>
+#home {
+  #hero {
     position: fixed;
     right: 0;
     bottom: 0;
-    min-width: 100%; 
+    min-width: 100%;
     min-height: 100%;
+  }
+  .wrapper-desc {
+    z-index: 1;
+    position: absolute;
+    margin-top: 20vh;
+    margin-left: 10vw;
+    font-size: 2rem;
+  }
 }
 </style>
