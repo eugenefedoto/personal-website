bf713e0c01d3da931465d7391b9f3d37ecdb68e8
Eugene Fedotov <eugenefedoto@gmail.com>
2018-10-09 00:03:43 -0500

    Remove background gradient.
    It was causing banding.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
         <nav class="tabs">
             <div class="selector"></div>
             <router-link to="/projects" class="uppercase">projects</router-link>
-            <router-link to="/hacking" class="uppercase">misc</router-link>
         </nav>
     </div>
     <router-view/>
@@ -14,40 +13,35 @@
 
 <style lang="scss">
 @import "~normalize.css";
-html{
-background: #0f0c29;  /* fallback for old browsers */
-background: -webkit-linear-gradient(to bottom, #24243e, #302b63, #0f0c29);  /* Chrome 10-25, Safari 5.1-6 */
-background: linear-gradient(to bottom, #24243e, #302b63, #0f0c29); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-}
-
-.wrapper-nav {
-  font-family: Nobile, sans-serif;
-  font-size: 2rem;
-  display: flex;
-  margin-top: 5vh;
-  margin-left: 1vw;
-  #logo {
-    margin: 0 2vw;
-  }
-  .tabs {
-    font-size: 1.1rem;
-    background: #e2e2e2;
-    box-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, 0.1);
-    border-radius: 0 1rem 1rem 0;
-    a {
-      text-decoration: none;
-      color: #777;
-      text-transform: uppercase;
-      padding: 0.5em;
-      display: inline-block;
-      &:hover {
-        color: #ff006a;
+body {
+  background: #2e3138;
+  .wrapper-nav {
+    font-family: Nobile, sans-serif;
+    font-size: 2rem;
+    display: flex;
+    margin-top: 5vh;
+    margin-left: 1vw;
+    #logo {
+      margin: 0 2vw;
+    }
+    .tabs {
+      font-size: 1.1rem;
+      background: #e2e2e2;
+      box-shadow: 0.1rem 0.1rem 0.3rem rgba(0, 0, 0, 0.1);
+      border-radius: 0 1rem 1rem 0;
+      a {
+        text-decoration: none;
+        color: #777;
+        text-transform: uppercase;
+        padding: 0.5em;
+        display: inline-block;
+        &:hover {
+          color: #ff006a;
+        }
       }
     }
   }
-}
-.subject,
+  .subject,
   #logo {
     text-decoration: none;
     background: linear-gradient(to right, #30cfd0 0%, #6410cc 100%);
@@ -62,4 +56,5 @@ background: linear-gradient(to bottom, #24243e, #302b63, #0f0c29); /* W3C, IE 10
       text-transform: uppercase;
     }
   }
+}
 </style>
